fd9f7f48d66b8d26956e76760954758abb517f8a
Sanaullah <sana.ullah@gmail.com>
2022-03-31 12:22:26 +0400

    added req location btn

--- src/components/Scan/Scan.js
+++ src/components/Scan/Scan.js
@@ -1,26 +1,110 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Grid, Paper, Box, Slide, Typography, Button } from '@material-ui/core';
 import { useStyles } from './scan.styles';
 
 const Scan = () => {
   const classes = useStyles();
+  // User's geolocation
+  const [geo, setGeo] = useState({lat: 0, long: 0});
 
   const reqCam = () => {
     if("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices){
       console.log("Ask camera access.");
 
       navigator.mediaDevices.getUserMedia({video: true})
-      .then(() => alert("Permission Granted !"))
-      .catch(() => alert("Permission Denied !"));
+      .then(() => alert("Camera Permission Granted !"))
+      .catch((err) => {
+        alert("Camera Permission Denied !");
+        console.log(err.message);
+      });
+    }
+  }
+  
+  const reqLoc = () => {
+    console.log("Loc start");
+    if("geolocation" in navigator && "getCurrentPosition" in navigator.geolocation){
+      console.log("Ask location access.");
+      
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setGeo({
+          lat: pos.coords.latitude,
+          long: pos.coords.longitude
+        });
+        console.log("Lat: ", pos.coords.latitude);
+        console.log("Long: ", pos.coords.longitude);
+        
+      }, (err) => {
+        console.log("Location err: ", err.message);
+      });
+
     }
   }
   
 
-  useEffect(() => {
-    reqCam();
-  }, []);
+  // useEffect(() => {
+  //   reqCam();
+  // }, []);
+
   return (
     <Container className={classes.scan}>
+
+      <Slide in={true} direction="up">
+        <Paper className={classes.infoBox}>
+          <Grid container className={classes.infoContainer}>
+            <Grid item className={classes.infoHead}>
+              <Typography
+                noWrap
+                variant="h5"
+                className={classes.infoHeadTxt}
+              >
+                Ask camera permission
+              </Typography>
+              <Box>
+                <label htmlFor="cam1">
+                  <Button 
+                    variant="contained" 
+                    color="primary"
+                    className={classes.edit}
+                    component="span"
+                    onClick={reqCam}
+                  >
+                    camera req
+                  </Button>
+                </label>
+              </Box>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Slide>
+      
+      <Slide in={true} direction="up">
+        <Paper className={classes.infoBox}>
+          <Grid container className={classes.infoContainer}>
+            <Grid item className={classes.infoHead}>
+              <Typography
+                noWrap
+                variant="h5"
+                className={classes.infoHeadTxt}
+              >
+                Lat: {geo.lat}, long: {geo.long}
+              </Typography>
+              <Box>
+                <label htmlFor="loc1">
+                  <Button 
+                    variant="contained" 
+                    color="primary"
+                    className={classes.edit}
+                    component="span"
+                    onClick={reqLoc}
+                  >
+                    location
+                  </Button>
+                </label>
+              </Box>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Slide>
 
       <Slide in={true} direction="up">
         <Paper className={classes.infoBox}>
